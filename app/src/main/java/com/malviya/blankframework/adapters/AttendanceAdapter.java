3869935fb99a8812731a43c2ce6cc3d761208dd5
malviya.software@gmail.com <123456>
2017-02-06 00:02:50 +0530

    updated code

---
+++ app/src/main/java/com/malviya/blankframework/adapters/AttendanceAdapter.java
@@ -0,0 +1,66 @@
+package com.malviya.blankframework.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.malviya.blankframework.R;
+import com.malviya.blankframework.models.AttendanceDataModel;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Admin on 05-02-2017.
+ */
+
+public class AttendanceAdapter extends RecyclerView.Adapter<AttendanceAdapter.MyViewHolder> {
+    private Context mContext;
+    private ArrayList<AttendanceDataModel> mList;
+
+    public AttendanceAdapter(Context context, ArrayList<AttendanceDataModel> pList) {
+        mContext = context;
+        mList = pList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        //View view = inflater.inflate(R.layout.attendance_row, null);
+        //if use null then recyclerview not take match_parent as width
+        View viewHolder = inflater.inflate(R.layout.attendance_row, parent,false);
+        //viewHolder.setLayoutParams(new RecyclerView.LayoutParams(RecyclerView.LayoutParams.MATCH_PARENT, RecyclerView.LayoutParams.WRAP_CONTENT));
+        return new MyViewHolder(viewHolder);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        //holder.textViewSubject.setText("" + mList.get(position).getSubjectValue() + "%");
+       // holder.textViewTotalDays.setText("" + mList.get(position).getTotalDays());
+       // holder.textViewabsentDays.setText("" + mList.get(position).getAbsentDays());
+    }
+
+
+    @Override
+    public int getItemCount()
+    {
+        return 10;
+       // return mList.size();
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView textViewSubject;
+        public TextView textViewTotalDays;
+        public TextView textViewabsentDays;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            textViewSubject = (TextView) itemView.findViewById(R.id.textview_attendance_row_subject_value);
+            textViewTotalDays = (TextView) itemView.findViewById(R.id.textview_attendance_row_total_value);
+            textViewabsentDays = (TextView) itemView.findViewById(R.id.textview_attendance_row_absent_value);
+        }
+    }
+}
--- app/src/main/java/com/malviya/blankframework/fragments/AttendanceFragment.java
+++ app/src/main/java/com/malviya/blankframework/fragments/AttendanceFragment.java
@@ -4,19 +4,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.malviya.blankframework.R;
+import com.malviya.blankframework.activities.DashboardActivity;
+import com.malviya.blankframework.adapters.AttendanceAdapter;
+import com.malviya.blankframework.models.AttendanceDataModel;
 import com.malviya.blankframework.utils.Utils;
+
+import java.util.ArrayList;
 
 /**
  * Created by Admin on 24-12-2016.
  */
 
 public class AttendanceFragment extends Fragment implements View.OnClickListener {
-    public final static String TAG="AttendanceFragment";
+    public final static String TAG = "AttendanceFragment";
+    private RecyclerView mRecycleViewAttendance;
+    private ArrayList<AttendanceDataModel> mAttendanceList;
+    private AttendanceAdapter mAttendanceAdapter;
+    private TextView mTextViewCourse;
+    private TextView mTextViewTerm;
+
     public AttendanceFragment() {
 
     }
@@ -24,6 +39,11 @@ public class AttendanceFragment extends Fragment implements View.OnClickListener
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        init();
+    }
+
+    private void init() {
+        mAttendanceList = new ArrayList<AttendanceDataModel>();
     }
 
     @Nullable
@@ -42,8 +62,30 @@ public class AttendanceFragment extends Fragment implements View.OnClickListener
     }
 
     private void initView() {
-
+        //------------------------------------
+        TextView mTextViewTitle = (TextView) getView().findViewById(R.id.textview_title);
+        mTextViewTitle.setText(R.string.tab_attendance);
+        ImageView mImgProfile = (ImageView) getView().findViewById(R.id.imageview_profile);
+        mImgProfile.setVisibility(View.VISIBLE);
+        mImgProfile  = DashboardActivity.mImgProfile;
+        ImageView mImgBack = (ImageView) getView().findViewById(R.id.imageview_back);
+        mImgBack.setVisibility(View.VISIBLE);
+        mImgBack.setOnClickListener(this);
+        //------------------------------------
+        initRecyclerView();
+        mTextViewCourse = (TextView) getView().findViewById(R.id.textview_attendance_course_value);
+        mTextViewTerm = (TextView) getView().findViewById(R.id.textview_attendance_term_value);
         setListener();
+    }
+
+    private void initRecyclerView() {
+        mRecycleViewAttendance = (RecyclerView) getView().findViewById(R.id.recyclerview_attendance);
+        mRecycleViewAttendance.setHasFixedSize(true);
+        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        manager.setSmoothScrollbarEnabled(true);
+        mRecycleViewAttendance.setLayoutManager(manager);
+        mAttendanceAdapter = new AttendanceAdapter(getContext(), mAttendanceList);
+        mRecycleViewAttendance.setAdapter(mAttendanceAdapter);
     }
 
     private void setListener() {
@@ -54,7 +96,9 @@ public class AttendanceFragment extends Fragment implements View.OnClickListener
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-
+            case R.id.imageview_back:
+                getActivity().onBackPressed();
+                break;
         }
     }
 
