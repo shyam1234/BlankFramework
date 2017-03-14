a674632bbb055e5df3c9aaaa2f59a613e71a7feb
malviya.software@gmail.com <123456>
2017-03-14 19:25:59 +0530

    updated code room

--- app/src/main/java/com/malviya/blankframework/adapters/EventsAdapter.java
+++ app/src/main/java/com/malviya/blankframework/adapters/EventsAdapter.java
@@ -12,7 +12,6 @@ import android.widget.TextView;
 import com.malviya.blankframework.R;
 import com.malviya.blankframework.models.TableNewsMasterDataModel;
 import com.malviya.blankframework.utils.AppLog;
-import com.malviya.blankframework.utils.GetPicassoImage;
 
 import java.util.ArrayList;
 
@@ -43,19 +42,19 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.MyViewHold
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
         AppLog.log(TAG," +++ onBindViewHolder ++++ ");
-        //String url ="https://edurpstorage.blob.core.windows.net/edurpcontainer/DEV/1/61/103?sv=2015-12-11&sr=b&sig=kPNs9zkQw0v1bqiNnvBkpjRY40ve6qm0%2BDak3zl26Xk%3D&se=2017-03-04T14%3A27%3A34Z&sp=rwl&rscd=attachment%3B%20filename%3Dimages.jpg";
-       // GetPicassoImage.getImage(mContext,url,holder.imageViewRhumbnil);
-        GetPicassoImage.getImage(mContext,mList.get(position).getThumbNailPath(),holder.imageViewRhumbnil);
-        holder.textViewPublishBy.setText(mList.get(position).getPublishedBy());
-        holder.textViewRefTitle.setText(mList.get(position).getReferenceTitle());
-        holder.textViewPublishedTime.setText(mList.get(position).getPublishedOn());
-        holder.webviewShortBody.loadData(mList.get(position).getShortBody(),"text/html; charset=utf-8", "utf-8");
-        holder.textViewTag.setText(mList.get(position).getMenuCode());
-        holder.textViewLike.setText(mList.get(position).getTotalLikes());
-        holder.textViewComment.setText(mList.get(position).getTotalComments());
-        //--------------------------------------------------------------
-        holder.imageViewRhumbnil.setOnClickListener(mListener);
-        holder.imageViewRhumbnil.setTag(position);
+//        //String url ="https://edurpstorage.blob.core.windows.net/edurpcontainer/DEV/1/61/103?sv=2015-12-11&sr=b&sig=kPNs9zkQw0v1bqiNnvBkpjRY40ve6qm0%2BDak3zl26Xk%3D&se=2017-03-04T14%3A27%3A34Z&sp=rwl&rscd=attachment%3B%20filename%3Dimages.jpg";
+//       // GetPicassoImage.getImage(mContext,url,holder.imageViewRhumbnil);
+//        GetPicassoImage.getImage(mContext,mList.get(position).getThumbNailPath(),holder.imageViewRhumbnil);
+//        holder.textViewPublishBy.setText(mList.get(position).getPublishedBy());
+//        holder.textViewRefTitle.setText(mList.get(position).getReferenceTitle());
+//        holder.textViewPublishedTime.setText(mList.get(position).getPublishedOn());
+//        holder.webviewShortBody.loadData(mList.get(position).getShortBody(),"text/html; charset=utf-8", "utf-8");
+//        holder.textViewTag.setText(mList.get(position).getMenuCode());
+//        holder.textViewLike.setText(mList.get(position).getTotalLikes());
+//        holder.textViewComment.setText(mList.get(position).getTotalComments());
+//        //--------------------------------------------------------------
+//        holder.imageViewRhumbnil.setOnClickListener(mListener);
+//        holder.imageViewRhumbnil.setTag(position);
     }
 
 
@@ -87,7 +86,6 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.MyViewHold
             textViewPublishBy = (TextView) itemView.findViewById(R.id.textview_news_row_published_by);
             textViewPublishedTime = (TextView) itemView.findViewById(R.id.textview_news_row_published_time);
             webviewShortBody = (WebView) itemView.findViewById(R.id.webview_news_row_shortbody);
-
             imageViewTag = (ImageView) itemView.findViewById(R.id.imageview_bottom_tag);
             imageViewLike = (ImageView) itemView.findViewById(R.id.imageview_bottom_like);
             imageViewComment = (ImageView) itemView.findViewById(R.id.imageview_bottom_comment);
